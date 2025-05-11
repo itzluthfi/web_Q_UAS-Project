f4f7e640692f28872d83560e962df8848e476985
itzluthfi <luthfishidqi2@gmail.com>
2025-05-11 19:34:48 +0700

    update: final sidebar

--- resources/views/components/sidebarAdmin.blade.php
+++ resources/views/components/sidebarAdmin.blade.php
@@ -66,17 +66,18 @@
             </a>
             
             <a href="#" class="sidebar-link flex items-center px-4 py-3 text-gray-300 hover:text-white">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
-                    <path d="M9 4.804A7.968 7.968 0 005.5 4c-1.255 0-2.443.29-3.5.804v10A7.969 7.969 0 015.5 14c1.669 0 3.218.51 4.5 1.385A7.962 7.962 0 0114.5 14c1.255 0 2.443.29 3.5.804v-10A7.968 7.968 0 0014.5 4c-1.255 0-2.443.29-3.5.804V12a1 1 0 11-2 0V4.804z" />
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 24 24" fill="currentColor">
+                    <path d="M12 2C6.477 2 2 5.954 2 10.5c0 2.114.898 4.067 2.392 5.522C4.23 17.152 3.272 19.272 3.05 19.786a.75.75 0 00.949.949c.514-.222 2.634-1.18 3.764-1.342A10.548 10.548 0 0012 19c5.523 0 10-3.954 10-8.5S17.523 2 12 2z" />
                 </svg>
-                <span class="sidebar-text">Manga</span>
+                <span class="sidebar-text">Comment</span>
             </a>
+
             
             <div class="px-4 py-2 mt-4 sidebar-group-label">
                 <h5 class="text-xs font-semibold text-gray-400 uppercase tracking-wider">Sistem</h5>
             </div>
             
-            <a href="#" class="sidebar-link flex items-center px-4 py-3 text-gray-300 hover:text-white">
+            <a href="{{ route('admin.setting') }}" class="sidebar-link flex items-center px-4 py-3 text-gray-300 hover:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-3" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M11.49 3.17c-.38-1.56-2.6-1.56-2.98 0a1.532 1.532 0 01-2.286.948c-1.372-.836-2.942.734-2.106 2.106.54.886.061 2.042-.947 2.287-1.561.379-1.561 2.6 0 2.978a1.532 1.532 0 01.947 2.287c-.836 1.372.734 2.942 2.106 2.106a1.532 1.532 0 012.287.947c.379 1.561 2.6 1.561 2.978 0a1.533 1.533 0 012.287-.947c1.372.836 2.942-.734 2.106-2.106a1.533 1.533 0 01.947-2.287c1.561-.379 1.561-2.6 0-2.978a1.532 1.532 0 01-.947-2.287c.836-1.372-.734-2.942-2.106-2.106a1.532 1.532 0 01-2.287-.947zM10 13a3 3 0 100-6 3 3 0 000 6z" clip-rule="evenodd" />
                 </svg>
